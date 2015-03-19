a1362b6bfdb8ec523bb2c4389e4d0f225991d9a4
Matt Giguere <matthew.giguere@gmail.com>
2015-03-19 11:11:04 -0400

    test columns
    tests were added to ensure that all 4 routines that extract the
    data return the correct number of columns in the returned pandas
    DataFrames. This ensures that all 4 data sets are still available
    and the paths and filenames haven't changed.

--- tests/test_electricPrice.py
+++ tests/test_electricPrice.py
@@ -9,6 +9,7 @@ import sys
 import argparse
 import pandas as pd
 import re
+from electric.code import electricPrice as ep
 
 __author__ = "Matt Giguere (github: @mattgiguere)"
 __maintainer__ = "Matt Giguere"
@@ -16,16 +17,29 @@ __email__ = "matthew.giguere@yale.edu"
 __status__ = " Development NOT(Prototype or Production)"
 __version__ = '0.0.1'
 
-from electric.code import electricPrice as ep
 
+#############################################
+#TEST NUMBER OF COLUMNS FOR ALL 4 DATA SETS
+#############################################
 def test_getEiaData_for_number_of_columns():
-	dfe = ep.getEiaData()
-	dfe_coln = len(dfe.columns.values)
-	assert dfe_coln == 11
+    dfe = ep.getEiaData()
+    dfe_coln = len(dfe.columns.values)
+    assert dfe_coln == 11
 
 
+def test_getUscbData_for_number_of_columns():
+    dfp = ep.getUscbData()
+    dfp_coln = len(dfp.columns.values)
+    assert dfp_coln == 8
 
 
+def test_getIncomeData_for_number_of_columns():
+    dfi = ep.getIncomeData()
+    dfi_coln = len(dfi.columns.values)
+    assert dfi_coln == 11
 
 
-	
+def test_getEiaRates_for_number_of_columns():
+    dfr = ep.getEiaRates()
+    dfr_coln = len(dfr.columns.values)
+    assert dfr_coln == 11
